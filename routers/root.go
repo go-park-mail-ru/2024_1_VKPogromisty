22853d919a49c332228ee8b60f5b40744222a612
Famelbyby <lexagorbunov14@gmail.com>
2024-03-02 22:37:15 +0300

    VKP-16-cors: исправить headers для CORS

--- routers/root.go
+++ routers/root.go
@@ -9,9 +9,10 @@ import (
 
 func SetUpCORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "localhost:3000")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
