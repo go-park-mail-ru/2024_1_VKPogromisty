403ee79418340da84a911495bbb090190a26adfe
PetrMitin <petr09mitin@mail.ru>
2024-03-05 19:23:57 +0300

    General: Добавить значения CORS-headers в конфиг

--- utils/config.go
+++ utils/config.go
@@ -1,6 +1,9 @@
 package utils
 
 const (
-	HOST = "localhost"
-	PORT = ":8080"
+	HOST            = "localhost"
+	PORT            = ":8080"
+	ALLOWED_ORIGIN  = "http://localhost:3000"
+	ALLOWED_HEADERS = "Accept, Accept-Language, Content-Type"
+	ALLOWED_METHODS = "GET, POST, PUT, DELETE, OPTIONS"
 )
