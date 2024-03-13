aea1fd0c6918bfa7fbd06fc7631cfa4c2c110038
PetrMitin <petr09mitin@mail.ru>
2024-03-13 20:46:04 +0300

    VKP-43-rk-mistakes: Fixed swagger deploy

--- main.go
+++ main.go
@@ -13,21 +13,21 @@ import (
 
 // swag init
 
-//	@title			Socio API
-//	@version		1.0
-//	@description	First version of Socio API
-//	@contact.name	Petr Mitin
-//	@contact.url	https://github.com/Petr09Mitin
-//	@contact.email	petr09mitin@mail.ru
-//	@host			localhost:8080
-//	@BasePath		/api/v1
+// @title			Socio API
+// @version		1.0
+// @description	First version of Socio API
+// @contact.name	Petr Mitin
+// @contact.url	https://github.com/Petr09Mitin
+// @contact.email	petr09mitin@mail.ru
+// @host			localhost:8080
+// @BasePath		/api/v1
 func main() {
 	rootRouter := routers.NewRootRouter()
 
 	http.Handle("/swagger/*", httpSwagger.Handler(
-		httpSwagger.URL("http://localhost:1234/swagger/doc.json"),
+		httpSwagger.URL("http://localhost:8001/swagger/doc.json"),
 	))
-	go http.ListenAndServe(":1234", nil)
+	go http.ListenAndServe(":8001", nil)
 
 	fmt.Printf("started on port %s\n", utils.PORT)
 	http.ListenAndServe(utils.PORT, rootRouter)
