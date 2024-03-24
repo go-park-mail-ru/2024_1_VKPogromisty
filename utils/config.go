5e48bb2fdbb271d1efe6be6e8355accb5ed773a9
Petr09Mitin <peter09mitin@mail.ru>
2024-03-24 12:34:32 +0300

    VKP-45-profile: Добавить swagger docs для получения профиля

--- utils/config.go
+++ utils/config.go
@@ -2,7 +2,7 @@ package utils
 
 const (
 	HOST            = "localhost"
-	PORT            = ":8081"
+	PORT            = ":8080"
 	ALLOWED_ORIGIN  = "http://localhost:3000" //"http://94.139.246.134"
 	ALLOWED_HEADERS = "Accept, Accept-Language, Content-Type"
 	ALLOWED_METHODS = "GET, POST, PUT, DELETE, OPTIONS"
