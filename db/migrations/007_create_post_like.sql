66bbbd67076d9505928f7e637df1ab66bf0565f3
PetrMitin <petr09mitin@mail.ru>
2024-04-24 01:11:23 +0300

    VKP-99-microservices: fix typo in migrations

--- db/migrations/007_create_post_like.sql
+++ db/migrations/007_create_post_like.sql
@@ -4,8 +4,8 @@ CREATE TABLE IF NOT EXISTS post_like (
   post_id BIGINT NOT NULL,
   user_id BIGINT NOT NULL,
   created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-  FOREIGN KEY (post_id) REFERENCES public.post (id) ON UPDATE CASCADE ON DELETE CASCADE
-  FOREIGN KEY (user_id) REFERENCES public.user (id) ON UPDATE CASCADE ON DELETE CASCADE
+  FOREIGN KEY (post_id) REFERENCES public.post (id) ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES public.user (id) ON UPDATE CASCADE ON DELETE CASCADE,
   CONSTRAINT post_like_post_user_unique UNIQUE (post_id, user_id)
 );
 
